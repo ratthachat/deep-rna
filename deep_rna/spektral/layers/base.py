1fb3292ccc89ecca721a5a0bdb64ab6292630d08
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2022-02-04 11:44:33 +0700

    add SimpleGCN

--- deep_rna/spektral/layers/base.py
+++ deep_rna/spektral/layers/base.py
@@ -38,3 +38,70 @@ class GraphMasking(Layer):
             inputs = inputs[..., :-1]
 
         return inputs
+
+
+class SimpleGCN(tf.keras.layers.Layer):
+    '''
+    Simplest Unnormalized GCN Convolutional Layer.
+    Easily use with other keras model's calling:
+  
+    # node_states, adjacency have to be explicitly given
+    ...
+    new_node_states = SimpleGCN(256)([node_states, adjacency])
+    new_node_states = Dense(128)(new_node_states)
+    ...
+    
+    '''
+    def __init__(
+        self,
+        units=128,
+        activation='linear',
+        kernel_initializer='glorot_uniform',
+        **kwargs
+    ):
+        self.units = units
+        self.activation = tf.keras.activations.get(activation)
+        self.kernel_initializer = kernel_initializer
+        self.supports_masking = True
+        
+        super().__init__(
+            **kwargs
+        )
+        
+    def build(self, input_shape):
+        node_input_dim = input_shape[0][-1]
+        self.kernel =  self.add_weight(
+            shape=(node_input_dim, self.units),
+            initializer=self.kernel_initializer,
+            name="kernel",
+        )
+
+        self.built = True
+    
+    def call(self, inputs, training=False, mask=None):
+        node_states, adjacency = inputs
+        node_states_aggregated = tf.matmul(adjacency, node_states)
+        node_states_aggregated = tf.matmul(node_states_aggregated, self.kernel)
+
+        if mask:
+            node_states_aggregated = self._apply_mask(node_states_aggregated, mask)
+
+        return self.activation(node_states_aggregated)
+
+    def compute_mask(self, inputs, mask=None):
+        if mask is None:
+            return None
+        return mask[0]
+
+    def _apply_mask(self, inputs, mask):
+        if len(tf.shape(inputs)) > len(tf.shape(mask[0])):
+            return inputs * tf.cast(tf.expand_dims(mask[0],axis=-1),inputs.dtype)
+        else:
+            return inputs * tf.cast(mask[0],inputs.dtype)
+    
+    def get_config(self):
+        base_config = super().get_config()
+        keras_config = {}
+        for key in self.kwargs_keys:
+            keras_config[key] = serialize_kwarg(key, getattr(self, key))
+        return {**base_config, **keras_config, **self.config}
