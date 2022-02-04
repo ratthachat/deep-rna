b0ec6e0e7f2e527f515973b55c6850e77bc85af0
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2022-02-04 17:59:03 +0700

    Create dataset.py
    initial commit, not usable

---
+++ deep_rna/dataset.py
@@ -0,0 +1,70 @@
+from deep_rna.spektral.data import Graph, Dataset
+
+class RNADataset(Dataset):
+    """
+    An upgraded Spektral's Batch Dataset which fully support multi-length RNA sequences
+    This RNADataset class assumes the user processes the data using Tutorial's notebook
+	https://www.kaggle.com/ratthachat/preprocessing-deep-learning-input-from-rna-string
+	
+	Essential input:
+	rna_seq_id_list: list of a unique rna-sequence id, must be the same used in the notebook above
+	node_dir: directory name containing node features generated from the notebook
+	edge_dir: directory name containing edge features generated from the notebook
+	
+	Optional input:
+	label_df: a pandas dataframe containing label information for each rna id in arbitrary format
+			  if label_df is not None, user must implement extract_label() to extract a label from
+			  seq_id and label_df
+	pred_len: length of each rna string to be predicted (i.e. have labels); just in case that
+			  pred_len < seq_len (as in OpenVaccine) for technical difficulties in bio-experiments
+    """
+
+    def __init__(self, 
+				 rna_seq_id_list,
+				 node_dir,
+				 edge_dir,
+				 label_df=None,
+                 pred_len=None,
+#                  package_names=['contrafold_2'], # TODO: auto-scan sub-dirs in edge_dir. Modify the tutorial notebook
+                 edge_thresh = 1e-1, 
+                 num_examples=None,
+                 **kwargs):
+		
+        self.label_df = label_df
+        self.edge_thresh = edge_thresh
+        self.pred_len = pred_len
+        self.training = training
+        self.package_name = package_name
+        self.num_examples = num_examples # optional: if you want to test only small data
+        self.__dict__.update(kwargs)
+		super().__init__(**kwargs)
+    
+    def extract_label(self, seq_id):
+        raise NotImplementedError
+    
+    def read(self):        
+        graph_list = []
+        for i in tqdm(range(len(rna_seq_id_list))):
+            seq_id = rna_seq_id_list[i]
+            node_feature_df = pd.read_csv(node_dir+f'{seq_id}_node_features.csv')
+            node_feature = node_feature_df.values # (seq_len, feat)
+            
+            edge_matrix = np.load(edge_dir + f"{self.package_name}_{seq_id}.npy")
+            edge_matrix = np.where(edge_matrix > self.edge_thresh, edge_matrix, 0)
+            
+            node_feature = node_feature[:self.pred_len]
+            edge_matrix = edge_matrix[:self.pred_len, :self.pred_len]
+            
+            if self.label_df is not None:
+                labels = self.extract_label(seq_id)
+                labels = labels[:self.pred_len] # TOFIX, allowing different len
+            else:
+                labels = None
+            
+           
+            graph_list.append(Graph(x=node_feature, a=edge_matrix, y=labels))
+            
+            if self.num_examples is not None and len(graph_list) == self.num_examples: 
+                break
+        
+        return graph_list
